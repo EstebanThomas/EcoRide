5c885a497b109319037224b34661616885c04318
EstebanThomas <et.thomas@hotmail.fr>
2025-04-24 10:15:26 +0200

    Create project laravel

---
+++ EcoRide/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!-- resources/views/home.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ma première page Laravel</title>
+</head>
+<body>
+    <h1>Bienvenue sur ma page Laravel !</h1>
+</body>
+</html>
