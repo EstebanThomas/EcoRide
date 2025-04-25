456f91979c255356ae5b9d36fd1d99f63b3fcc77
EstebanThomas <et.thomas@hotmail.fr>
2025-04-25 12:19:13 +0200

    Update responsive

--- EcoRide/resources/views/home.blade.php
+++ EcoRide/resources/views/home.blade.php
@@ -6,10 +6,10 @@
     <!--Form search route-->
     <div>
 
-        <form action="/Recherche-covoiturage" method="GET" class="flex flex-col">
+        <form action="/Recherche-covoiturage" method="GET" class="flex flex-col mt-8">
 
-            <div class="flex flex-row w-full justify-evenly">
-                <div class="relative top-[165px]">
+            <div class="flex flex-row w-full justify-center gap-4">
+                <div class="relative top-[260px]">
                     <!--<label for="depart">Départ</label>-->
                     <input
                         type="text"
@@ -17,12 +17,12 @@
                         name="depart"
                         placeholder="Départ"
                         required
-                        class="bg-green4 border-2 border-green1 rounded-[20px] pl-8 pr-2 w-[140px] h-[33px] focus:border-2 focus:border-green1 focus:outline focus:outline-green1 font-second text-[20px] uppercase text-center"
+                        class="bg-green4 border-2 border-green1 rounded-[20px] pl-8 pr-2 w-sm h-18 focus:border-2 focus:border-green1 focus:outline focus:outline-green1 font-second text-4xl uppercase text-center placeholder-black"
                     />
-                    <img src="{{ asset('images/Depart.svg') }}" alt="Logo Depart" class="absolute top-1/2 -translate-y-1/2 pl-2">
+                    <img src="{{ asset('images/Depart.svg') }}" alt="Logo Depart" class="w-15 h-15 absolute top-1/2 -translate-y-1/2 pl-2">
                 </div>
                 
-                <div class="relative top-[165px]">
+                <div class="relative top-[260px]">
                     <!--<label for="destination">Destination</label>-->
                     <input
                         type="text"
@@ -30,14 +30,14 @@
                         name="destination"
                         placeholder="Destination"
                         required
-                        class="bg-green4 border-2 border-green1 rounded-[20px] pl-8 pr-2 w-[140px] h-[33px] focus:border-2 focus:border-green1 focus:outline focus:outline-green1 font-second text-[20px] uppercase text-center"
+                        class="bg-green4 border-2 border-green1 rounded-[20px] pl-8 pr-2 w-sm h-18 focus:border-2 focus:border-green1 focus:outline focus:outline-green1 font-second text-4xl uppercase text-center placeholder-black"
                     />
-                    <img src="{{ asset('images/Arrivee.svg') }}" alt="Logo Destination" class="absolute top-1/2 -translate-y-1/2 pl-2">
+                    <img src="{{ asset('images/Arrivee.svg') }}" alt="Logo Destination" class="w-15 h-15 absolute top-1/2 -translate-y-1/2 pl-2">
                 </div>
             </div>
 
-            <div class="flex flex-row w-full justify-evenly">
-                <div class="relative top-[174px]">
+            <div class="flex flex-row w-full justify-center gap-4">
+                <div class="relative top-[275px]">
                     <!--<label for="date">Date</label>-->
                     <input
                         type="text"
@@ -45,12 +45,12 @@
                         name="date"
                         placeholder="Date"
                         required
-                        class="bg-green4 border-2 border-green1 rounded-[20px] pl-8 pr-2 w-[140px] h-[33px] focus:border-2 focus:border-green1 focus:outline focus:outline-green1 font-second text-[20px] uppercase text-center"
+                        class="bg-green4 border-2 border-green1 rounded-[20px] pl-8 pr-2 w-sm h-18 focus:border-2 focus:border-green1 focus:outline focus:outline-green1 font-second text-4xl uppercase text-center placeholder-black"
                     />
-                    <img src="{{ asset('images/Date.svg') }}" alt="Logo Date" class="absolute top-1/2 -translate-y-1/2 pl-2">
+                    <img src="{{ asset('images/Date.svg') }}" alt="Logo Date" class="w-15 h-15 absolute top-1/2 -translate-y-1/2 pl-2">
                 </div>
 
-                <div class="relative top-[174px]">
+                <div class="relative top-[275px]">
                     <!--<label for="passager">Passager</label>-->
                     <input
                         type="text"
@@ -58,16 +58,16 @@
                         name="passager"
                         placeholder="Passager"
                         required
-                        class="bg-green4 border-2 border-green1 rounded-[20px] pl-8 pr-2 w-[140px] h-[33px] focus:border-2 focus:border-green1 focus:outline focus:outline-green1 font-second text-[20px] uppercase text-center"
+                        class="bg-green4 border-2 border-green1 rounded-[20px] pl-8 pr-2 w-sm h-18 focus:border-2 focus:border-green1 focus:outline focus:outline-green1 font-second text-4xl uppercase text-center placeholder-black"
                     />
-                    <img src="{{ asset('images/NombreDePassager.svg') }}" alt="Logo nombre de passager" class="absolute top-1/2 -translate-y-1/2 pl-2">
+                    <img src="{{ asset('images/NombreDePassager.svg') }}" alt="Logo nombre de passager" class="w-15 h-15 absolute top-1/2 -translate-y-1/2 pl-2">
                 </div>
             </div>
 
             <div class="flex justify-center">
                 <button
-                class="relative bg-green4 border-2 border-green1 rounded-[20px] top-[190px] w-[140px] h-[33px] font-second text-[20px] uppercase text-center" type="submit">
-                <img src="{{ asset('images/Recherche.svg') }}" alt="Logo recherche" class="absolute top-1/2 -translate-y-1/2 pl-2">
+                class="relative bg-green4 border-2 border-green1 rounded-[20px] top-[290px] w-sm h-18 font-second text-4xl uppercase text-center" type="submit">
+                <img src="{{ asset('images/Recherche.svg') }}" alt="Logo recherche" class="w-15 h-15 absolute top-1/2 -translate-y-1/2 pl-2">
                 Chercher
                 </button>
             </div>
--- EcoRide/resources/views/partials/header.blade.php
+++ EcoRide/resources/views/partials/header.blade.php
@@ -3,13 +3,13 @@
     <!--Larger than Smartphone-->
     <div class="relative hidden xl:block">
 
-        <img src="{{ asset('images/EcoRide_Logo_WiBg.png') }}" alt="Logo" class="absolute flex w-[200px] h-[91px] left-[30px] top-1/2 -translate-y-1/2 2xl:left-[60px] 2xl:w-[259px] 2xl:h-[118px]">
+        <img src="{{ asset('images/EcoRide_Logo_WiBg.png') }}" alt="Logo" class="absolute max-h-none max-w-none w-[200px] h-[100px] left-[30px] top-1/2 -translate-y-1/2 2xl:left-[60px] 2xl:w-[259px] 2xl:h-[118px]">
 
         <nav class="absolute flex justify-evenly items-center w-[800px] h-[65] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-[20px]"> <!-- xs:top-[45px] left-1/2 and top-1/2 = put corner high right center and -translate-x-1/2 -translate-y-1/2 = recenter -->
-            <a class="font-second text-[24px] flex w-[200px] h-[55px] items-center justify-center flex-col" href="/"><img src="{{ asset('images/Accueil.svg') }}" alt="Logo Accueil">ACCUEIL</a>
-            <a class="font-second text-[24px] flex w-[200px] h-[55px] items-center justify-center flex-col" href="/covoiturages"><img src="{{ asset('images/Covoiturages.svg') }}" alt="Logo Covoiturages">COVOITURAGES</a>
-            <a class="font-second text-[24px] flex w-[200px] h-[55px] items-center justify-center flex-col" href="/connexion"><img src="{{ asset('images/Connexion.svg') }}" alt="Logo Connexion">CONNEXION</a>
-            <a class="font-second text-[24px] flex w-[200px] h-[55px] items-center justify-center flex-col" href="/contact"><img src="{{ asset('images/Contact.svg') }}" alt="Logo Contact">CONTACT</a>
+            <a class="font-second text-[24px] tracking-wide flex w-[200px] h-[55px] items-center justify-center flex-col" href="/"><img src="{{ asset('images/Accueil.svg') }}" alt="Logo Accueil">ACCUEIL</a>
+            <a class="font-second text-[24px] tracking-wide flex w-[200px] h-[55px] items-center justify-center flex-col" href="/covoiturages"><img src="{{ asset('images/Covoiturages.svg') }}" alt="Logo Covoiturages">COVOITURAGES</a>
+            <a class="font-second text-[24px] tracking-wide flex w-[200px] h-[55px] items-center justify-center flex-col" href="/connexion"><img src="{{ asset('images/Connexion.svg') }}" alt="Logo Connexion">CONNEXION</a>
+            <a class="font-second text-[24px] tracking-wide flex w-[200px] h-[55px] items-center justify-center flex-col" href="/contact"><img src="{{ asset('images/Contact.svg') }}" alt="Logo Contact">CONTACT</a>
         </nav>
 
         <div class="bg-green4 w-full h-[35px]"></div>
@@ -22,19 +22,19 @@
     <!--Smartphone-->
     <div class="relative block xl:hidden">
 
-        <img src="{{ asset('images/EcoRide_Logo_WiBg.png') }}" alt="Logo" class="absolute flex h-[83px] w-[187px] top-[45px] left-1/2 -translate-x-1/2">
+        <img src="{{ asset('images/EcoRide_Logo_WiBg.png') }}" alt="Logo" class="absolute w-[300px] h-[150px] top-[80px] left-1/2 -translate-x-1/2">
 
-        <nav class="absolute flex justify-evenly items-center w-[300px] h-[50px] top-[135px] left-1/2 -translate-x-1/2 bg-green4 rounded-[20px] border border-green1"> <!-- left-1/2 and top-1/2 = put corner high right center and -translate-x-1/2 -translate-y-1/2 = recenter -->
-            <a class="font-second text-[16px] flex w-[200px] h-[55px] items-center justify-center flex-col" href="/"><img src="{{ asset('images/Accueil.svg') }}" alt="Logo Accueil">ACCUEIL</a>
-            <a class="font-second text-[16px] flex w-[200px] h-[55px] items-center justify-center flex-col" href="/covoiturages"><img src="{{ asset('images/Covoiturages.svg') }}" alt="Logo Covoiturages">COVOITURAGES</a>
-            <a class="font-second text-[16px] flex w-[200px] h-[55px] items-center justify-center flex-col" href="/connexion"><img src="{{ asset('images/Connexion.svg') }}" alt="Logo Connexion">CONNEXION</a>
-            <a class="font-second text-[16px] flex w-[200px] h-[55px] items-center justify-center flex-col" href="/contact"><img src="{{ asset('images/Contact.svg') }}" alt="Logo Contact">CONTACT</a>
+        <nav class="absolute flex justify-evenly items-center h-[100px] w-[calc(100%-4rem)] top-[230px] left-1/2 -translate-x-1/2 bg-green4 rounded-[20px] border border-green1"> <!-- left-1/2 and top-1/2 = put corner high right center and -translate-x-1/2 -translate-y-1/2 = recenter -->
+            <a class="font-second text-4xl tracking-wide flex w-[200px] h-[55px] items-center justify-center flex-col" href="/"><img src="{{ asset('images/Accueil.svg') }}" alt="Logo Accueil" class="w-15 h-15">ACCUEIL</a>
+            <a class="font-second text-4xl tracking-wide flex w-[200px] h-[55px] items-center justify-center flex-col" href="/covoiturages"><img src="{{ asset('images/Covoiturages.svg') }}" alt="Logo Covoiturages" class="w-15 h-15">COVOITURAGES</a>
+            <a class="font-second text-4xl tracking-wide flex w-[200px] h-[55px] items-center justify-center flex-col" href="/connexion"><img src="{{ asset('images/Connexion.svg') }}" alt="Logo Connexion" class="w-15 h-15">CONNEXION</a>
+            <a class="font-second text-4xl tracking-wide flex w-[200px] h-[55px] items-center justify-center flex-col" href="/contact"><img src="{{ asset('images/Contact.svg') }}" alt="Logo Contact" class="w-15 h-15">CONTACT</a>
         </nav>
 
-        <div class="bg-green4 w-full h-[10px]"></div>
-        <div class="bg-green3 w-full h-[10px]"></div>
-        <div class="bg-green2 w-full h-[10px]"></div>
-        <div class="bg-green1 w-full h-[10px]"></div>
+        <div class="bg-green4 w-full h-[20px]"></div>
+        <div class="bg-green3 w-full h-[20px]"></div>
+        <div class="bg-green2 w-full h-[20px]"></div>
+        <div class="bg-green1 w-full h-[20px]"></div>
 
     </div>
 
